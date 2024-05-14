bc7a67f50716b4a64249dcb550ea305738b70c01
Linetus <dagoar3443@gmail.com>
2024-05-14 19:01:55 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -22,19 +22,21 @@ $(document).ready(function() {
 
 function procesarXML(xml) {
     var atracciones = [];
-    $(xml).find('Atraccion').each(function() {
-        var id = $(this).attr('id');
-        var nombre = $(this).find('nombre').text();
-        var areaTematica = $(this).closest('AreaTematica').find('nombreAT').text();
-        var tipo = $(this).find('tipo').text();
-        var intensidad = $(this).find('intensidad').text();
-        atracciones.push({
-            id: id,
-            nombre: nombre,
-            areaTematica: areaTematica,
-            tipo: tipo,
-            intensidad: intensidad,
-            tiempoEspera: null // Se llenará después con el JSON
+    $(xml).find('AreaTematica').each(function() {
+        var areaTematica = $(this).find('nombreAT').text();
+        $(this).find('Atraccion').each(function() {
+            var id = $(this).attr('id');
+            var nombre = $(this).find('nombre').text();
+            var tipo = $(this).find('tipo').text();
+            var intensidad = $(this).find('intensidad').text();
+            atracciones.push({
+                id: id,
+                nombre: nombre,
+                areaTematica: areaTematica,
+                tipo: tipo,
+                intensidad: intensidad,
+                tiempoEspera: null // Se llenará después con el JSON
+            });
         });
     });
     return atracciones;
@@ -52,16 +54,34 @@ function combinarDatos(atraccionesXML, atraccionesJSON) {
 
 function generarContenido(atracciones) {
     var contenido = '';
+    var areas = {};
+
+    // Agrupar atracciones por área temática
     for (var i = 0; i < atracciones.length; i++) {
-        contenido += '<div class="card mb-3">';
-        contenido += '<div class="card-body">';
-        contenido += '<h5 class="card-title">' + atracciones[i].nombre + '</h5>';
-        contenido += '<p class="card-text"><strong>Área Temática:</strong> ' + atracciones[i].areaTematica + '</p>';
-        contenido += '<p class="card-text"><strong>Tipo:</strong> ' + atracciones[i].tipo + '</p>';
-        contenido += '<p class="card-text"><strong>Intensidad:</strong> ' + atracciones[i].intensidad + '</p>';
-        contenido += '<p class="card-text"><strong>Tiempo de Espera:</strong> ' + (atracciones[i].tiempoEspera ? atracciones[i].tiempoEspera + ' minutos' : 'No disponible') + '</p>';
-        contenido += '</div>';
-        contenido += '</div>';
+        var area = atracciones[i].areaTematica;
+        if (!areas[area]) {
+            areas[area] = [];
+        }
+        areas[area].push(atracciones[i]);
     }
+
+    // Generar contenido agrupado por áreas temáticas
+    for (var area in areas) {
+        if (areas.hasOwnProperty(area)) {
+            contenido += '<h2 class="mt-5">' + area + '</h2>';
+            for (var j = 0; j < areas[area].length; j++) {
+                var atraccion = areas[area][j];
+                contenido += '<div class="card mb-3">';
+                contenido += '<div class="card-body">';
+                contenido += '<h5 class="card-title">' + atraccion.nombre + '</h5>';
+                contenido += '<p class="card-text"><strong>Tipo:</strong> ' + atraccion.tipo + '</p>';
+                contenido += '<p class="card-text"><strong>Intensidad:</strong> ' + atraccion.intensidad + '</p>';
+                contenido += '<p class="card-text"><strong>Tiempo de Espera:</strong> ' + (atraccion.tiempoEspera ? atraccion.tiempoEspera + ' minutos' : 'No disponible') + '</p>';
+                contenido += '</div>';
+                contenido += '</div>';
+            }
+        }
+    }
+
     $('#atracciones-list').html(contenido);
 }
